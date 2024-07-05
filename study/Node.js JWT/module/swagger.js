c007abd23d68f7cb903a1e2b62429bac95481ca8
kwangyoulsagong <sgky0511@naver.com>
2024-07-05 17:36:06 +0900

    modified: file

---
+++ study/Node.js JWT/module/swagger.js
@@ -0,0 +1,22 @@
+const swaggerUi = require('swagger-ui-express');
+const swaggereJsdoc = require('swagger-jsdoc');
+
+const options = {
+    swaggerDefinition: {
+        info: {
+            title: 'Test API',
+            version: '1.0.0',
+            description: 'Test API with express',
+        },
+        host: 'localhost:8000',
+        basePath: '/'
+    },
+    apis: ['../router/*.js', './swagger/*']
+};
+
+const specs = swaggereJsdoc(options);
+
+module.exports = {
+    swaggerUi,
+    specs
+};
