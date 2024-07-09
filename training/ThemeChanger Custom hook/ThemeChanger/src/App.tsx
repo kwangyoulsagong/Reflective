bef2365594088bdfd9cabe07b3757f40f5a1134c
kwangyoulsagong <sgky0511@naver.com>
2024-07-09 17:49:18 +0900

    feat: context api를 활용해 테마 변경 상태 관리 적용

--- training/ThemeChanger Custom hook/ThemeChanger/src/App.tsx
+++ training/ThemeChanger Custom hook/ThemeChanger/src/App.tsx
@@ -1,13 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Main from './component/main'
-
+import { ThemeProvider, useTheme } from './Provider/theme'
 function App() {
-
-
+  const {theme}=useTheme()
   return (
-       <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-        <Main/>
+    
+       <div className={`flex flex-col items-center justify-center min-h-screen ${theme}`}>
+          <Main/>
       </div>
+
   )
 }
 
--- training/ThemeChanger Custom hook/ThemeChanger/src/component/theme.tsx
+++ training/ThemeChanger Custom hook/ThemeChanger/src/component/theme.tsx
@@ -1,19 +1,75 @@
-const Theme=({onCLose}:any)=>{
-    return(
+import React from "react";
+import { useTheme } from "../Provider/theme";
+
+const Theme = ({ onCLose }: any) => {
+  const { setTheme } = useTheme();
+
+  // 테마 변경 함수
+  const handleTheme = (value: string) => {
+    setTheme(value);
+    onCLose()
+  };
+
+  return (
     <div className="fixed inset-0 flex justify-center items-center bg-gray-900 bg-opacity-50">
-        <div className="bg-white rounded-lg p-6 grid grid-cols-4 gap-4">
-            <button className="border border-gray-300 p-3 rounded-md hover:bg-gray-100 focus:outline-none">Button 1</button>
-            <button className="border border-gray-300 p-3 rounded-md hover:bg-gray-100 focus:outline-none">Button 2</button>
-            <button className="border border-gray-300 p-3 rounded-md hover:bg-gray-100 focus:outline-none">Button 3</button>
-            <button className="border border-gray-300 p-3 rounded-md hover:bg-gray-100 focus:outline-none">Button 4</button>
-            <button className="border border-gray-300 p-3 rounded-md hover:bg-gray-100 focus:outline-none">Button 5</button>
-            <button className="border border-gray-300 p-3 rounded-md hover:bg-gray-100 focus:outline-none">Button 6</button>
-            <button className="border border-gray-300 p-3 rounded-md hover:bg-gray-100 focus:outline-none">Button 7</button>
-            <button className="border border-gray-300 p-3 rounded-md hover:bg-gray-100 focus:outline-none">Button 8</button>
-        </div>
-        <button className="absolute top-4 right-4 p-2 bg-red-500 text-white rounded-md hover:bg-red-600 focus:outline-none" onClick={onCLose}>닫기</button>
+      <div className="bg-white rounded-lg p-6 grid grid-cols-4 gap-4">
+        <button
+          className="border border-gray-300 p-3 rounded-md bg-gray-100 hover:bg-gray-200 focus:outline-none"
+          onClick={() => handleTheme("bg-gray-100")}
+        >
+          그레이
+        </button>
+        <button
+          className="border border-gray-300 p-3 rounded-md bg-blue-200 hover:bg-blue-100 focus:outline-none"
+          onClick={() => handleTheme("bg-blue-200")}
+        >
+          하늘색
+        </button>
+        <button
+          className="border border-gray-300 p-3 rounded-md bg-slate-600 hover:bg-slate-500 text-slate-300 focus:outline-none"
+          onClick={() => handleTheme("bg-slate-600")}
+        >
+          슬레이트
+        </button>
+        <button
+          className="border border-gray-300 p-3 rounded-md bg-teal-300 hover:bg-teal-100 text-slate-500 focus:outline-none"
+          onClick={() => handleTheme("bg-teal-300")}
+        >
+          민트
+        </button>
+        <button
+          className="border border-gray-300 p-3 rounded-md bg-purple-300 hover:bg-purple-100 text-white focus:outline-none"
+          onClick={() => handleTheme("bg-purple-300")}
+        >
+          보라색
+        </button>
+        <button
+          className="border border-gray-300 p-3 rounded-md bg-rose-300 hover:bg-rose-200 text-white focus:outline-none"
+          onClick={() => handleTheme("bg-rose-300")}
+        >
+          연분홍
+        </button>
+        <button
+          className="border border-gray-300 p-3 rounded-md bg-orange-500 hover:bg-orange-300 focus:outline-none"
+          onClick={() => handleTheme("bg-orange-500")}
+        >
+          오랜지
+        </button>
+        <button
+          className="border border-gray-300 p-3 rounded-md bg-yellow-400 hover:bg-yellow-300 text-white focus:outline-none"
+          onClick={() => handleTheme("bg-yellow-400")}
+        >
+          노란색
+        </button>
+      </div>
+      <button
+        className="absolute top-4 right-4 p-2 bg-red-500 text-white rounded-md hover:bg-red-600 focus:outline-none"
+        onClick={onCLose}
+      >
+        닫기
+      </button>
     </div>
-      
-    )
-}
-export default Theme
+  );
+};
+
+export default Theme;
