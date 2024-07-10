6655afe6a9363ce28dbda6910d54149966d6bfd6
kwangyoulsagong <sgky0511@naver.com>
2024-07-10 14:22:33 +0900

    feat: 리액트 todo 미션

---
+++ Personal Mission/mission3/React-Todo/src/App.tsx
@@ -0,0 +1,15 @@
+import React, { useEffect } from 'react'
+
+
+function App() {
+
+  return (
+    <>
+       <div className="p-4 bg-blue-500 text-white rounded">
+          Hello, Tailwind CSS!
+      </div>
+    </>
+  )
+}
+
+export default App
