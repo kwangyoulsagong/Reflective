babb6898711c9889ef18fb9bf1bf6df03b5f7f18
kwangyoulsagong <sgky0511@naver.com>
2024-07-18 11:49:39 +0900

    feat: 프로필 모델 구현

--- Personal Mission/mission4/using react nodejs blog/backend/model/profileModel.ts
+++ Personal Mission/mission4/using react nodejs blog/backend/model/profileModel.ts
@@ -1,7 +1,24 @@
-import mongoose, { Document, Schema } from "mongoose";
+import mongoose, { Document, Schema, Types } from "mongoose";
 
 
-//프로필 인터페이스 정의
+// 프로필 인터페이스 정의
 export interface IProfile extends Document{
-    
+    profile_id: mongoose.Types.ObjectId,
+    user_id: mongoose.Types.ObjectId,
+    image_url:string,
+    created_date:Date
+    updated_date:Date
 }
+
+// 프로필 스키마
+const profileSchema: Schema<IProfile>= new Schema({
+    profile_id: {type:Schema.Types.ObjectId, required:true, auto:true},
+    user_id: {type: Schema.Types.ObjectId, required:true, ref:'User'},
+    image_url: {type: String, default:null},
+    created_date: {type: Date, required:true, default:Date.now},
+    updated_date: {type: Date, required:true, default:Date.now}
+})
+
+const Profile=mongoose.model<IProfile>("Profile", profileSchema)
+
+export default Profile
