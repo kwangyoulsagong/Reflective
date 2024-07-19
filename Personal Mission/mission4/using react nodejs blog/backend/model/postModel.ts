673c95d93991c07d223819a4da7b0e5298a04f3a
kwangyoulsagong <sgky0511@naver.com>
2024-07-19 13:46:26 +0900

    feat: 게시물 모델 추가

--- Personal Mission/mission4/using react nodejs blog/backend/model/postModel.ts
+++ Personal Mission/mission4/using react nodejs blog/backend/model/postModel.ts
@@ -1,1 +1,25 @@
 import mongoose, { Document, Schema, Types } from "mongoose";
+
+// 게시물 인터페이스 정의
+export interface IPost extends Document{
+    post_id:mongoose.Types.ObjectId,
+    user_id: mongoose.Types.ObjectId,
+    title: string,
+    contents:any,
+    like_count:Number,
+    created_date:Date,
+    updated_date:Date
+}
+// 게시물 스키마 
+const postSchema:Schema<IPost>=new Schema({
+    post_id:{type: Schema.Types.ObjectId, required:true, auto: true },
+    user_id:{type: Schema.Types.ObjectId, required: true, ref:"User"},
+    title:{type: String, required:true },
+    contents:{type:Schema.Types.Mixed, required:true},
+    created_date:{type: Date, required:true, default:Date.now() },
+    updated_date:{type: Date, required:true, default:Date.now() }
+})
+
+const Post = mongoose.model<IPost>("Post", postSchema);
+
+export default Post
