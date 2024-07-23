be83946d371c433f5cb5fe4ef6a3d1a5b73f00e0
kwangyoulsagong <sgky0511@naver.com>
2024-07-23 15:02:23 +0900

    feat: 댓글 조회 서비스 기능: 댓글 조회를 하기위해 서비스 로직으로 데이터베이스에 접그하여 댓글, 유저 닉네임, 유저 프로필 이미지 조회

--- Personal Mission/mission4/using react nodejs blog/backend/service/commentService.ts
+++ Personal Mission/mission4/using react nodejs blog/backend/service/commentService.ts
@@ -1,4 +1,6 @@
 import Comment, {IComment} from "../model/commentModel";
+import Profile from "../model/profileModel";
+import User from "../model/userModel";
 
 class CommentService{
 
@@ -13,6 +15,38 @@ class CommentService{
             return await comment.save();
         }
         return null;
+    }
+
+    // 댓글 조회 서비스
+    public async getComment(post_id:string):Promise<object[] | null>{
+        try{
+            const comments= await Comment.find({post_id}).exec()
+            if(!comments) return null
+            
+            const commentsUserData=await Promise.all(
+                comments.map(async (comment) =>{
+                    // 유저 조회
+                    const user = await User.findOne({user_id:comment.user_id}).exec()
+                    // 유저 프로필 이미지 조회
+                    const profile = await Profile.findOne({user_id: comment.user_id}).exec()
+
+                    if( user&& profile){
+                        return {
+                            ...comment.toObject(),
+                            nickname: user.nickname,
+                            image_url:profile.image_url
+                        }
+                    }
+                    return comment.toObject()
+                })
+            )
+            return commentsUserData
+        }
+        catch(error:any){
+            console.error("댓글 조회 에러", error);
+            return null;
+        }
+
     }
 }
 
